997a0f0feb70bc9c5f130e9046f8ee2cc674b9d9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FilterOnDateRanges/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using DevExpress.Data.Filtering;
+using DevExpress.Xpf.Editors;
+
+namespace FilterOnDateRanges {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+
+            List<TestData> list = new List<TestData>();
+            for (int month = 1; month <= 12; month++)
+                for (int day = 1; day <= 28; day++)
+                    list.Add(new TestData() {
+                        CurDate = new DateTime(DateTime.Today.Year, month, day)
+                    });
+            grid.DataSource = list;
+        }
+    }
+
+    public class TestData {
+        public DateTime CurDate {
+            get;
+            set;
+        }
+    }
+
+    public class DateEditFilter : ContentControl {
+        public static readonly DependencyProperty FilterProperty;
+        static DateEditFilter() {
+            FilterProperty = DependencyProperty.Register("Filter", typeof(CriteriaOperator),
+                typeof(DateEditFilter), new PropertyMetadata(null));
+        }
+
+        public CriteriaOperator Filter {
+            get {
+                return (CriteriaOperator)GetValue(FilterProperty);
+            }
+            set {
+                SetValue(FilterProperty, value);
+            }
+        }
+
+        DateEdit dateEditFrom;
+        DateEdit dateEditTo;
+        public override void OnApplyTemplate() {
+            base.OnApplyTemplate();
+
+            if (dateEditFrom != null)
+                dateEditFrom.EditValueChanged -=
+                    new EditValueChangedEventHandler(dateEdit_EditValueChanged);
+            if (dateEditTo != null)
+                dateEditTo.EditValueChanged -=
+                    new EditValueChangedEventHandler(dateEdit_EditValueChanged);
+
+            dateEditFrom = FindName("PART_DateEditFrom") as DateEdit;
+            dateEditTo = FindName("PART_DateEditTo") as DateEdit;
+
+            GroupOperator op = Filter as GroupOperator;
+            if (op != null) {
+                dateEditFrom.EditValue =
+                    ((op.Operands[0] as BinaryOperator).RightOperand as OperandValue).Value;
+                dateEditTo.EditValue =
+                    ((op.Operands[1] as BinaryOperator).RightOperand as OperandValue).Value;
+            }
+
+            dateEditTo.EditValueChanged +=
+                new EditValueChangedEventHandler(dateEdit_EditValueChanged);
+            dateEditFrom.EditValueChanged +=
+                new EditValueChangedEventHandler(dateEdit_EditValueChanged);
+        }
+
+        void dateEdit_EditValueChanged(object sender, EditValueChangedEventArgs e) {
+            Filter = CriteriaOperator.And(new BinaryOperator("CurDate", dateEditFrom.EditValue, BinaryOperatorType.GreaterOrEqual),
+                                          new BinaryOperator("CurDate", dateEditTo.EditValue, BinaryOperatorType.LessOrEqual));
+        }
+
+
+    }
+}
---
+++ VB/FilterOnDateRanges/MainPage.xaml.vb
@@ -0,0 +1,91 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Xpf.Editors
+
+Namespace FilterOnDateRanges
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+
+			Dim list As New List(Of TestData)()
+			For month As Integer = 1 To 12
+				For day As Integer = 1 To 28
+					list.Add(New TestData() With {.CurDate = New DateTime(DateTime.Today.Year, month, day)})
+				Next day
+			Next month
+			grid.DataSource = list
+		End Sub
+	End Class
+
+	Public Class TestData
+		Private privateCurDate As DateTime
+		Public Property CurDate() As DateTime
+			Get
+				Return privateCurDate
+			End Get
+			Set(ByVal value As DateTime)
+				privateCurDate = value
+			End Set
+		End Property
+	End Class
+
+	Public Class DateEditFilter
+		Inherits ContentControl
+		Public Shared ReadOnly FilterProperty As DependencyProperty
+		Shared Sub New()
+			FilterProperty = DependencyProperty.Register("Filter", GetType(CriteriaOperator), GetType(DateEditFilter), New PropertyMetadata(Nothing))
+		End Sub
+
+		Public Property Filter() As CriteriaOperator
+			Get
+				Return CType(GetValue(FilterProperty), CriteriaOperator)
+			End Get
+			Set(ByVal value As CriteriaOperator)
+				SetValue(FilterProperty, value)
+			End Set
+		End Property
+
+		Private dateEditFrom As DateEdit
+		Private dateEditTo As DateEdit
+		Public Overrides Sub OnApplyTemplate()
+			MyBase.OnApplyTemplate()
+
+			If dateEditFrom IsNot Nothing Then
+				RemoveHandler dateEditFrom.EditValueChanged, AddressOf dateEdit_EditValueChanged
+			End If
+			If dateEditTo IsNot Nothing Then
+				RemoveHandler dateEditTo.EditValueChanged, AddressOf dateEdit_EditValueChanged
+			End If
+
+			dateEditFrom = TryCast(FindName("PART_DateEditFrom"), DateEdit)
+			dateEditTo = TryCast(FindName("PART_DateEditTo"), DateEdit)
+
+			Dim op As GroupOperator = TryCast(Filter, GroupOperator)
+			If op IsNot Nothing Then
+				dateEditFrom.EditValue = (TryCast((TryCast(op.Operands(0), BinaryOperator)).RightOperand, OperandValue)).Value
+				dateEditTo.EditValue = (TryCast((TryCast(op.Operands(1), BinaryOperator)).RightOperand, OperandValue)).Value
+			End If
+
+			AddHandler dateEditTo.EditValueChanged, AddressOf dateEdit_EditValueChanged
+			AddHandler dateEditFrom.EditValueChanged, AddressOf dateEdit_EditValueChanged
+		End Sub
+
+		Private Sub dateEdit_EditValueChanged(ByVal sender As Object, ByVal e As EditValueChangedEventArgs)
+			Filter = CriteriaOperator.And(New BinaryOperator("CurDate", dateEditFrom.EditValue, BinaryOperatorType.GreaterOrEqual), New BinaryOperator("CurDate", dateEditTo.EditValue, BinaryOperatorType.LessOrEqual))
+		End Sub
+
+
+	End Class
+End Namespace
